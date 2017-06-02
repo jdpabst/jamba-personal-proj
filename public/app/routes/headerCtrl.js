440cb16139fcf027ad1672ebe4cea4346276d7ba
Jessica Hollingshead <Jessica Hollingshead>
2017-06-02 11:42:12 -0600

    finished

--- public/app/routes/headerCtrl.js
+++ public/app/routes/headerCtrl.js
@@ -10,11 +10,13 @@ angular.module('app').controller('headerCtrl', function($scope){
             $(".mobile_menu_dropdown").css("top", "-517px")
             $(".home_main_container").css("padding-top", "0")
             $(".main_menu_section").css("padding-top", "0")
+            $(".main_smoothie_section").css("padding-top", "0")
             open = false;
         } else {
             $(".mobile_menu_dropdown").css("top", "59px")
             $(".home_main_container").css("padding-top", "518px")
             $(".main_menu_section").css("padding-top", "518px")
+            $(".main_smoothie_section").css("padding-top", "518px")
             open = true;
         }
 
--- public/app/routes/upbeet/upbeetCtrl.js
+++ public/app/routes/upbeet/upbeetCtrl.js
@@ -1,4 +1,23 @@
 angular.module('app').controller('upbeetCtrl', function($scope, upbeetService){
+$scope.smallTbl =  true;
+$scope.mediumTbl = false;
+$scope.largeTbl = false;
+
+$scope.changeNutrition = function(str){
+    if(str === 'small'){
+        $scope.smallTbl =  true;
+        $scope.mediumTbl = false;
+        $scope.largeTbl = false;
+    } else if (str === 'medium'){
+        $scope.smallTbl = false;
+        $scope.mediumTbl = true;
+        $scope.largeTbl = false;
+    } else if (str === 'large'){
+        $scope.smallTbl =  false;
+        $scope.mediumTbl = false;
+        $scope.largeTbl = true;
+    }
+}
 
     $scope.getUpbeet = function(){
         upbeetService.getUpbeet().then(function(res){
@@ -9,4 +28,26 @@ angular.module('app').controller('upbeetCtrl', function($scope, upbeetService){
     }
 
     $scope.getUpbeet();
+
+    let nutritionSmall = $(".upbeet_small")
+    let nutritionMedium = $(".upbeet_medium")
+    let nutritionLarge = $(".upbeet_large")
+
+    nutritionSmall.click(function(){
+        nutritionSmall.addClass("upbeet_active")
+        nutritionMedium.removeClass("upbeet_active")
+        nutritionLarge.removeClass("upbeet_active")
+    })
+
+    nutritionMedium.click(function(){
+        nutritionMedium.addClass("upbeet_active")
+        nutritionSmall.removeClass("upbeet_active")
+        nutritionLarge.removeClass("upbeet_active")
+    })
+
+    nutritionLarge.click(function(){
+        nutritionLarge.addClass("upbeet_active")
+        nutritionMedium.removeClass("upbeet_active")
+        nutritionSmall.removeClass("upbeet_active")
+    })
 })
